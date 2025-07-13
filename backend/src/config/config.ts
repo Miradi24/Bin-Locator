8f21fa7b948060ad23c75ae9ba97ad589096555c
Miradi Ombala <Miradiombala2002@gmail.com>
2025-07-13 13:38:38 +0100

    base backend setup

---
+++ backend/src/config/config.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+
+// Load environment variables from a .env file into current process
+dotenv.config();
+
+interface IConfig {
+    port: number;
+    nodeEnv: string;
+}
+
+const config: IConfig = {
+    port: Number(process.env.PORT) || 3000,
+    nodeEnv: process.env.NODE_ENV ?? 'development',
+};
+
+export default config;
