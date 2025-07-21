ccc694e828d541659ef20955d814462fde40cd26
Miradi Ombala <Miradiombala2002@gmail.com>
2025-07-21 12:07:49 +0100

    enable connection to local database

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -6,11 +6,13 @@ dotenv.config();
 interface IConfig {
     port: number;
     nodeEnv: string;
+    databaseUrl: string;
 }
 
 const config: IConfig = {
     port: Number(process.env.PORT) || 3000,
     nodeEnv: process.env.NODE_ENV || 'development',
+    databaseUrl: process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.6'
 };
 
 export default config;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,26 +2,41 @@ import express from 'express';
 import morgan from 'morgan';
 import config from './config/config';
 import { markerRouter } from './routes/marker.route';
+import mongoose from 'mongoose';
 
-const app = express();
+async function startServer() {
+    try {
+        await mongoose.connect(config.databaseUrl);
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.error('Database connection failed:', error);
+        // Stop the whole application if the database connection fails
+        process.exit(1);
+    }
 
-// Server configuration
+    const app = express();
 
-// The server will only parse JSON requests.
-app.use(express.json());
-// The server will also parse URL-encoded requests.
-app.use(express.urlencoded({ extended: true }));
-// Log prettier output to the console.
-app.use(morgan('dev'));
+    // Server configuration
 
-// Routes
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+    // The server will only parse JSON requests.
+    app.use(express.json());
+    // The server will also parse URL-encoded requests.
+    app.use(express.urlencoded({ extended: true }));
+    // Log prettier output to the console.
+    app.use(morgan('dev'));
 
-app.use('/api/markers', markerRouter);
+    // Routes
+    app.get('/', (req, res) => {
+        res.send('Hello, World!');
+    });
 
-// Start the server
-app.listen(config.port, () => {
-    console.log(`Server is running on http://localhost:${config.port} in ${config.nodeEnv} mode`);
-});
+    // Register the marker router for handling marker related API requests
+    app.use('/api/markers', markerRouter);
+
+    // Start the server
+    app.listen(config.port, () => {
+        console.log(`Server is running on http://localhost:${config.port} in ${config.nodeEnv} mode`);
+    });
+}
+
+startServer();
