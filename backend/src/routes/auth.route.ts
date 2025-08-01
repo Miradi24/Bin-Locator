784d318dad481c19b1fabaca7c05bc4bb46d9b7b
Miradi Ombala <Miradiombala2002@gmail.com>
2025-08-01 12:15:27 +0100

    remove unused controllers

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,9 +1,7 @@
 import { Router } from "express";
-import { isSessionActive, login, logout, register } from "../controllers/auth.controller";
+import { login, register } from "../controllers/auth.controller";
 
 export const authRouter = Router();
 
 authRouter.route("/login").post(login);
 authRouter.route("/register").post(register);
-authRouter.route("/logout").post(logout);
-authRouter.route('/session').get(isSessionActive)
